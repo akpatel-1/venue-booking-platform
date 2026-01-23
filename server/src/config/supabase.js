e29df5132e14b28da04a51c83d83dca09175cd29
akpatel-1 <akpatel.work1@gmail.com>
2026-01-23 10:32:59 +0530

    setup: configure Supabase client

---
+++ server/src/config/supabase.js
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+import 'dotenv/config'
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SECRET_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase environment variables are missing");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
