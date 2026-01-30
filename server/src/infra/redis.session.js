5e58dd3a7b9a036bbe8aa0237655700aa31fe08f
akpatel-1 <akpatel.work1@gmail.com>
2026-01-30 05:39:30 +0530

    feat: implement admin session infrastructure

---
+++ server/src/infra/redis.session.js
@@ -0,0 +1,21 @@
+import { redis } from "../config/redis.js";
+import crypto from "crypto";
+
+export async function createRedisSession(adminId, role = "admin") {
+  const SESSION_TTL = 60 * 60 * 24;
+  const sessionId = crypto.randomUUID();
+  const sessionKey = `session:${sessionId}`;
+  const sessionData = {
+    adminId,
+    role,
+    createdAt: Date.now(),
+  };
+  await redis.set(sessionKey, sessionData, {
+    ex: SESSION_TTL,
+  });
+  return sessionId;
+}
+
+export async function getSessionData(sessionId) {
+  return redis.get(`session:${sessionId}`);
+}
