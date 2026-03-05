01944ff230cf35aaa9c6346367c08759838afb92
akpatel-1 <akpatel.work1@gmail.com>
2026-03-05 09:23:09 +0530

    fix(vendor-routes): remove requireVendorAccess middleware from application status route

--- server/src/routes/vendor.routes.js
+++ server/src/routes/vendor.routes.js
@@ -3,7 +3,6 @@ import express from 'express';
 import { handleGetVendorStatus } from '../controllers/vendor/vendor.verification.controller.js';
 import { ensureAccountActive } from '../middlewares/shared/account.guard.js';
 import { requireAuth } from '../middlewares/shared/auth.middleware.js';
-import { requireVendorAccess } from '../middlewares/vendor/vendor.guard.js';
 
 export const vendorRoutes = express.Router();
 
@@ -11,6 +10,5 @@ vendorRoutes.get(
   '/partners/application/status',
   requireAuth,
   ensureAccountActive,
-  requireVendorAccess,
   handleGetVendorStatus
 );
