f0286edd00b092e85bcfb76b1315dde4f422d260
akpatel-1 <akpatel.work1@gmail.com>
2026-08-19 16:42:59 +0530

    refactor: improve upload flag handling in processApplication function

--- server/src/modules/user/application/service.js
+++ server/src/modules/user/application/service.js
@@ -31,9 +31,10 @@ export async function getApplicationStatus(userId) {
 export async function processApplication(userId, userData, userFile) {
   const fileExtension = path.extname(userFile.originalname);
   const documentKey = `vendor-application/${userId}/${Date.now()}-pan${fileExtension}`;
+  let upload = false;
   try {
     await uploadToR2(userFile.buffer, documentKey, userFile.mimetype);
-
+    upload = true;
     return await withTransaction(pool, async (client) => {
       return await insertVendorApplication(client, {
         userId,
@@ -42,7 +43,7 @@ export async function processApplication(userId, userData, userFile) {
       });
     });
   } catch (err) {
-    if (documentKey) {
+    if (upload) {
       await deleteFromR2(documentKey);
     }
     throw err;
