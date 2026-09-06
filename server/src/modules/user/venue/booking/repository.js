e8a6a418061d8727e6871cc28d964188ade156d9
akpatel-1 <akpatel.work1@gmail.com>
2026-09-06 11:57:20 +0530

    fix: specify table alias in WHERE clause for getVenues query

--- server/src/modules/user/venue/booking/repository.js
+++ server/src/modules/user/venue/booking/repository.js
@@ -5,7 +5,7 @@ export async function getVenues() {
         SELECT v.id, v.vendor_id, v.name, v.category, v.district, v.state, v.booking_type, v.opening_time, v.closing_time, MIN(vp.price) AS starting_price 
         FROM venues v
         JOIN venue_pricing vp ON vp.venue_id = v.id
-        WHERE status = 'live'
+        WHERE v.status = 'live'
         GROUP BY v.id, v.vendor_id, v.name, v.category, v.district, v.state, v.booking_type, v.opening_time, v.closing_time
     `);
   return result.rows;
